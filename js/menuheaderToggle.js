586828d946463b06798e230cb80945509588fa53
David <davidgabrielss11@gmail.com>
2023-10-18 22:25:14 -0300

    Update - Filtro de peças.

--- js/menuheaderToggle.js
+++ js/menuheaderToggle.js
@@ -2,3 +2,9 @@ function menuheaderToggle(){
     const toggleMenuHeader = document.querySelector('.menuheader');
     toggleMenuHeader.classList.toggle('active')
 }
+
+function menufilter(){
+
+    const filtermenu = document.querySelector('.menu-filter')
+    filtermenu.classList.toggle('active')
+}
